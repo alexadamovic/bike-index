f66da2c736cec2ed602054c014a58400eeaab49e
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-09 13:49:40 -0800

    add initial code for error checking
    Co-Authored By: Kevin Williams <kevin@k9w.org>

--- src/index.js
+++ src/index.js
@@ -11,9 +11,13 @@ function clearFields () {
 }
 
 function getElements(response) {
-  if (response.bikes) {
-    console.log(response);
+  console.log(response);
+  if (response.bikes.length === 0) {
+    $('#show-bikes').text(`No bikes have been reported stolen within 10 miles of this zip code.`);
+  } else if (response.bikes) {
     $('#show-bikes').text(`The last bike that was stolen in your zipcode is a ${response.bikes[0].frame_colors.toString()} ${response.bikes[0].frame_model} ${response.bikes[0].manufacturer_name}`);
+  } else {
+    $('#show-bikes').text(`Oopsie! Something went wrong on our end.`);
   }
 }
 
